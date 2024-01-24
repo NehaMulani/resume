7c0d3cebf6da7a16fa9f3876c31b64d9223768c4
NehaMulani <157102528+NehaMulani@users.noreply.github.com>
2024-01-24 13:55:19 +0530

    Add files via upload

---
+++ resume1/src/test/java/com/example/resume1/resume1/Resume1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume1.resume1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resume1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
